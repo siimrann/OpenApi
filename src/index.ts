ddcc9a16d84255da6df60106bec7a082ee1cbd6b
Simran Tyagi <tyagisimran938@gmail.com>
2024-11-14 08:46:55 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { createRoute } from "@hono/zod-openapi";
+import { ParamsSchema } from "./input";
+import { UserSchema } from "./output";
+const app = new OpenAPIHono();
+
+const route = createRoute({
+  method: "get",
+  path: "/users/{id}",
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: UserSchema,
+        },
+      },
+      description: "Retrieve the user",
+    },
+  },
+});
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id,
+    age: 20,
+    name: "Ultra-man",
+  });
+});
+
+export default app;
